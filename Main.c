f7d7f77082788a84dfecfe6669bf056e73b8c1fd
degaart <degaart@users.noreply.github.com>
2026-05-01 16:12:17 +0300

    feat: project skeleton

---
+++ Main.c
@@ -0,0 +1,136 @@
+/*
+ *
+ * ┌──────────────────────┐ ┌─────────────────────────────────┐
+ * │         Add          │ │                                 │
+ * └──────────────────────┘ │                                 │
+ * ┌──────────────────────┐ │                                 │
+ * │    Generate Code     │ │                                 │
+ * └──────────────────────┘ │                                 │
+ * ┌─Layout───────────────┐ │                                 │
+ * │ + root               │ │                                 │
+ * │   + timerLabel       │ │        LAYOUT PREVIEW           │
+ * │   + buttonContainer  │ │                                 │
+ * │     + startButton    │ │                                 │
+ * │     + stopButton     │ │                                 │
+ * │     + resetButton    │ │                                 │
+ * └──────────────────────┘ │                                 │
+ * ┌─Properties───────────┐ │                                 │
+ * │ Width:         [   ] │ │                                 │
+ * │ Height:        [   ] │ │                                 │
+ * │ Align:  [ CENTER  v] │ │                                 │
+ * │ Wrap:   [ NO_WRAP v] │ │                                 │
+ * └──────────────────────┘ └─────────────────────────────────┘
+ *
+*/
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+#include <stdbool.h>
+#include "flex.h"
+
+#define APPNAME "TimeTracker"
+
+typedef struct
+{
+    HINSTANCE hInstance;
+} AppState;
+
+static bool OnCreate(HWND hwnd, AppState* appState)
+{
+    SetWindowLongPtrA(hwnd, GWLP_USERDATA, (LONG_PTR)appState);
+
+    RECT rc;
+    GetClientRect(hwnd, &rc);
+
+    return true;
+}
+
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    switch (msg)
+    {
+    case WM_CREATE:
+        if (OnCreate(hwnd, (AppState*)(((CREATESTRUCTA*)lparam)->lpCreateParams)))
+        {
+            return 0;
+        }
+        break;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        return 0;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProcA(hwnd, msg, wparam, lparam);
+}
+
+static bool InitApplication(HINSTANCE hInstance)
+{
+    WNDCLASSEXA wc = {0};
+    wc.cbSize = sizeof(wc);
+    wc.hInstance = hInstance;
+    wc.lpszClassName = APPNAME;
+    wc.hCursor = LoadCursorA(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE+1);
+    wc.lpfnWndProc = WindowProc;
+    return RegisterClassExA(&wc);
+}
+
+static bool InitInstance(HINSTANCE hInstance, INT nShowCmd, AppState* appState)
+{
+    unsigned style = WS_OVERLAPPEDWINDOW|WS_CLIPCHILDREN|WS_THICKFRAME;
+
+    RECT rcWindow = {
+        .top = 0,
+        .left = 0,
+        .right = 320,
+        .bottom = 240,
+    };
+    AdjustWindowRectEx(&rcWindow, style, FALSE, 0);
+
+    HWND hwnd = CreateWindowA(
+            APPNAME, 
+            APPNAME,
+            style,
+            CW_USEDEFAULT, 0,
+            rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top,
+            NULL,
+            NULL,
+            hInstance,
+            appState);
+    if (!hwnd)
+    {
+        return false;
+    }
+    ShowWindow(hwnd, nShowCmd);
+    UpdateWindow(hwnd);
+    return true;
+}
+
+INT WINAPI WinMain(HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpCmdLine,
+                    INT nShowCmd)
+{
+    AppState appState = {0};
+    appState.hInstance = hInstance;
+
+    if (!InitApplication(hInstance))
+    {
+        return 1;
+    }
+    else if (!InitInstance(hInstance, nShowCmd, &appState))
+    {
+        return 1;
+    }
+
+    MSG msg;
+    while (GetMessageA(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+    return (int)msg.wParam;
+}
+
